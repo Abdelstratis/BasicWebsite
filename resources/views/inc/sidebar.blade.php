bb861923c6a2f4f27e4dcfc2a8bce15cdf722ad4
Abdelstratis <36842994+Abdelstratis@users.noreply.github.com>
2018-03-23 17:24:46 +0100

    Update sidebar.blade.php

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -5,6 +5,23 @@
         <h3>Sidebar</h3>
 
         This is the sidebar
+        <hr>
+        <center>
+        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header">Inscrivez-Vous...</div>
+            <div class="card-body">
+                <h5 class="card-title">Newsletter</h5>
+                <p class="card-text">Pour rester au top des nouveautés</p>
+                <form  action="" method="post">
+                    <input class="form-control" type="text" name="news" placeholder="Votre Mail ">
+                    <br>
+
+                    <input type="submit" class="btn  btn-primary btn-block">
+                </form>
+            </div>
+        </div>
+    <hr>
+        </center>
 
     </div>
 @show
