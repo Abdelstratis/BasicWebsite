393d41a8b22833078cc0bff244101a2aee2a25a4
AbdelBelkhiri <AbdelBelkhiri@AbdelBelkhiri.fr>
2018-03-23 16:42:57 +0100

    oui

---
+++ resources/views/incorrect.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="alert alert-danger" role="alert">
+        Mot de passe incorrect. Veuillez réessayer !
+    </div>
+    <a href="http://localhost/BasicWebsite/public/auth" class="btn btn-primary btn-lg btn-block"> Réessayez  </a>
+@stop
